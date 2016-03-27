2526851bd8159e3899750deaf5972cd03e9b43cb
Kannan C <ckannan759153@hotmail.com>
2016-03-28 01:59:00 +0530

    Initial

---
+++ Sivas/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sivas.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public ProductCategory Category { get; set; }
+        public byte[] Image { get; set; }
+        public bool Landscape { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public ProductEnergyStar EnergyStar { get; set; }
+        public string Color { get; set; }
+        public string Specification { get; set; }
+    }
+    public enum ProductCategory
+    {
+        Firdge,Wood
+    }
+    public enum ProductEnergyStar
+    {
+        One,Two,Three,Four,Five
+    }
+}
