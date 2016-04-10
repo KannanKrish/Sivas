cba7fba348ede8ec62815a2380aa74a71c91e373
Kannan C <ckannan759153@hotmail.com>
2016-04-10 11:47:00 +0530

    Initial

---
+++ Sivas/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Sivas.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Sivas.Models.SivasContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Sivas.Models.SivasContext";
+        }
+
+        protected override void Seed(Sivas.Models.SivasContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- Sivas/Models/Products.cs
+++ Sivas/Models/Products.cs
@@ -9,20 +9,24 @@ namespace Sivas.Models
     {
         public int Id { get; set; }
         public ProductCategory Category { get; set; }
-        public byte[] Image { get; set; }
-        public bool Landscape { get; set; }
+        public string Company { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+        public byte[] Image { get; set; }
+        public bool Landscape { get; set; }
         public ProductEnergyStar EnergyStar { get; set; }
+        public int Price { get; set; }
+        public bool Offer { get; set; }
         public string Color { get; set; }
         public string Specification { get; set; }
+        public string Description { get; set; }
     }
     public enum ProductCategory
     {
-        Firdge,Wood,AC
+        Firdge, Wood, AC
     }
     public enum ProductEnergyStar
     {
-        One,Two,Three,Four,Five
+        One, Two, Three, Four, Five
     }
 }
